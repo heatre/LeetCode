f56b97a8e6ce05bef11c4d63dde80bdf9ba1701c
Swin Zhang <heatre@163.com>
2016-03-08 17:38:24 +0800

    addTwoNum方法

--- src/AddTwoNumbers2.java
+++ src/AddTwoNumbers2.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import com.sun.org.apache.xerces.internal.impl.dv.ValidatedInfo;
 
 /**
@@ -18,23 +21,63 @@ public class AddTwoNumbers2
 {
 	public class ListNode//内部类，定义链表数据结构
 	{
-		 int val;
-		 ListNode next;
-		 ListNode(int x)
-		 {
-			 val = x;
-		 }
-		
+		int val;
+		ListNode next;
+		ListNode(int x)
+		{
+			val = x;
+		}
+
 	}
 	public ListNode addTwoNumbers(ListNode l1,ListNode l2)
 	{
-		
-		return null;
+		if(null == l1 || null == l2)
+			return null;
+		ListNode node = new ListNode(0);
+		ListNode headNode = node;
+		int sum =0;
+		int carry = 0;
+		int tmp = 0;
+		while(l1 != null || l2 != null || carry != 0)
+		{
+			tmp = (l1 != null ? l1.val:0) + (l2 != null ? l2.val:0)+carry;
+			sum = tmp%10;
+			carry = tmp/10;
+			node.next = new ListNode(sum);
+			node = node.next;
+			l1 = (l1 != null) ? l1.next : null;
+			l2 = (l2 != null) ? l2.next : null;
+		}
+
+		return headNode.next;
 	}
-	
+
 	public static void main(String[] args)
 	{
+		AddTwoNumbers2 adn = new AddTwoNumbers2();
+		ListNode l1 = adn.new ListNode(0);
+		ListNode l2 = adn.new ListNode(0);
 
+		ListNode node = l1;
+		for(int i =0;i<5;i++)
+		{
+			node.next = adn.new ListNode(i);
+			node = node.next;
+		}
+
+		node = l2;
+		for(int i =3;i>0;i--)
+		{
+			node.next = adn.new ListNode(i);
+			node = node.next;
+		}
+
+		node = adn.addTwoNumbers(l1.next, l2.next);
+		while(node!= null)
+		{
+			System.out.println(node.val);
+			node = node.next;
+		}
 	}
 
 }
