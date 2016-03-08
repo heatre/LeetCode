86ea5e7c00d061522d0c0b7b3eef2266c03dd840
Swin Zhang <heatre@163.com>
2016-03-08 16:30:00 +0800

    添加pm2的解题方法

---
+++ src/AddTwoNumbers2.java
@@ -0,0 +1,41 @@
+import com.sun.org.apache.xerces.internal.impl.dv.ValidatedInfo;
+
+/**
+ * 2. Add Two Numbers
+ * You are given two linked lists representing two non-negative numbers. 
+ * The digits are stored in reverse order and each of their nodes contain a single digit. 
+ * Add the two numbers and return it as a linked list.
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ *
+ * @author Swin
+ *
+ */
+
+
+
+public class AddTwoNumbers2
+{
+	public class ListNode//内部类，定义链表数据结构
+	{
+		 int val;
+		 ListNode next;
+		 ListNode(int x)
+		 {
+			 val = x;
+		 }
+		
+	}
+	public ListNode addTwoNumbers(ListNode l1,ListNode l2)
+	{
+		
+		return null;
+	}
+	
+	public static void main(String[] args)
+	{
+
+	}
+
+}
+
--- src/TwoSum1.java
+++ src/TwoSum1.java
@@ -1,17 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 /**
+ * 1. Two Sum
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
  *
- *Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- *You may assume that each input would have exactly one solution.
-  Example:
-  Given nums = [2, 7, 11, 15], target = 9,
-
-  Because nums[0] + nums[1] = 2 + 7 = 9,
-  return [0, 1].
-  
  * @author Swin
  * 使用hashmap的算法复杂度为O（n）
  */
